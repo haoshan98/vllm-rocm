c28db46d5805a89810b9c3acbd95aec9009a61f4
haoshan98 <haoshanw@gmail.com>
2023-12-19 17:10:51 +0800

    Codspeed setup

--- codspeed_action.sh
+++ codspeed_action.sh
@@ -1,16 +1,16 @@
 # Configure and run codspeed-runner
 
-# Get the runner arguments
-RUNNER_ARGS=""
-if [ -n "be5d5cd5-e053-4b63-8c53-8c95cc84e907" ]; then
-  RUNNER_ARGS="$RUNNER_ARGS --token be5d5cd5-e053-4b63-8c53-8c95cc84e907"
-fi
-if [ -n "" ]; then
-  RUNNER_ARGS="$RUNNER_ARGS --working-directory=./"
-fi
-if [ -n "" ]; then
-  RUNNER_ARGS="$RUNNER_ARGS --upload-url="
-fi
+# # Get the runner arguments
+# RUNNER_ARGS=""
+# if [ -n "be5d5cd5-e053-4b63-8c53-8c95cc84e907" ]; then
+#   RUNNER_ARGS="$RUNNER_ARGS --token be5d5cd5-e053-4b63-8c53-8c95cc84e907"
+# fi
+# if [ -n "" ]; then
+#   RUNNER_ARGS="$RUNNER_ARGS --working-directory=./"
+# fi
+# if [ -n "" ]; then
+#   RUNNER_ARGS="$RUNNER_ARGS --upload-url="
+# fi
 
 echo "set RUNNER_ARGS"
 
@@ -26,4 +26,5 @@ fi
 echo "install CodSpeedHQ/runner"
 
 # Run the benchmarks
-codspeed-runner $RUNNER_ARGS -- 'pytest tests/kernels/test_layernorm.py --codspeed'
+# codspeed-runner $RUNNER_ARGS -- 'pytest tests/kernels/test_layernorm.py --codspeed'
+codspeed-runner --token be5d5cd5-e053-4b63-8c53-8c95cc84e907 -- 'pytest tests/kernels/test_layernorm.py --codspeed'
